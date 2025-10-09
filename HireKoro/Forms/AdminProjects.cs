b04c58646e817ae194a1613acddd17fccb4dfd9d
Fahim-shad <fahimshad136@gmail.com>
2025-10-10 00:42:17 +0600

    AdminProjects

--- HireKoro/Forms/AdminProjects.cs
+++ HireKoro/Forms/AdminProjects.cs
@@ -18,11 +18,10 @@ namespace HireKoro.Forms
             this.LoadProjects();
         }
 
-        internal void LoadProjects()
+        internal void LoadProjects(string query = "SELECT P.ProjectID, P.Title, P.ProjectStatus, P.ProjectHours,  P.StartDate,  P.Cost,   P.ProjectDescription,  C.Name AS ClientName,  F.Name AS FreelancerName FROM PROJECTS P JOIN USERS C ON P.ClientID = C.UserID LEFT JOIN USERS F ON P.FreelancerID = F.UserID;")
         {
             try
             {
-                string query = "SELECT P.ProjectID, P.Title, P.ProjectStatus, P.ProjectHours,  P.StartDate,  P.Cost,   P.ProjectDescription,  C.Name AS ClientName,  F.Name AS FreelancerName FROM PROJECTS P JOIN USERS C ON P.ClientID = C.UserID LEFT JOIN USERS F ON P.FreelancerID = F.UserID;";
                 var response = Main.DB.ExecuteQueryTable(query);
                 dgvProjects.DataSource = response;
             }
@@ -30,6 +29,44 @@ namespace HireKoro.Forms
             {
                 MessageBox.Show("Error loading projects: " + ex.Message);
             }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string query = $"SELECT P.ProjectID, P.Title, P.ProjectStatus, P.ProjectHours,  P.StartDate,  P.Cost,   P.ProjectDescription,  C.Name AS ClientName,  F.Name AS FreelancerName FROM PROJECTS P JOIN USERS C ON P.ClientID = C.UserID LEFT JOIN USERS F ON P.FreelancerID = F.UserID where ProjectID like '{this.txtSearch.Text}%' or Title like '{this.txtSearch.Text}%';";
+            this.LoadProjects(query);
+        }
+
+        internal void Clearall()
+        {
+            this.txtProjectID.Clear();
+            this.txtClientID.Clear();
+            this.txtFreelancerID.Clear();
+            this.txtStatus.Clear();
+            this.txtTitle.Clear();
+            this.txtDescription.Clear();
+            this.txtHours.Clear();
+            this.txtCost.Clear();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.Clearall();
+        }
+
+        private void dgvProjects_DoubleClick(object sender, EventArgs e)
+        {
+            this.txtProjectID.Text = this.dgvProjects.CurrentRow.Cells["ProjectID"].Value.ToString();
+            this.txtClientID.Text = this.dgvProjects.CurrentRow.Cells["ClientID"].Value.ToString();
+            this.txtFreelancerID.Text = this.dgvProjects.CurrentRow.Cells["FreelancerID"].Value.ToString();
+            this.txtStatus.Text = this.dgvProjects.CurrentRow.Cells["ProjectStatus"].Value.ToString();
+            this.txtTitle.Text = this.dgvProjects.CurrentRow.Cells["Title"].Value.ToString();
+            this.txtDescription.Text = this.dgvProjects.CurrentRow.Cells["ProjectDescription"].Value.ToString();
+            this.txtHours.Text = this.dgvProjects.CurrentRow.Cells["ProjectHours"].Value.ToString();
+            this.txtCost.Text = this.dgvProjects.CurrentRow.Cells["Cost"].Value.ToString();
+
+
         }
     }
 }
