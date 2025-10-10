e1c1aec4dc3772613f1c10fb4fdbf98957e0250a
Fahim-shad <fahimshad136@gmail.com>
2025-10-11 01:33:39 +0600

    AdminInvoices , AdminUsers

--- HireKoro/Forms/AdminInvoices.cs
+++ HireKoro/Forms/AdminInvoices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,11 @@ namespace HireKoro.Forms
         {
             InitializeComponent();
             this.LoadInvoices();
+            this.load_ProjectName();
+            
+            
+
+
         }
 
         internal void LoadInvoices(string query = "select I.InvoiceID , P.Title , I.Status , I.Amount , I.IssueDate , I.DueDate  from INVOICES AS I , PROJECTS AS P WHERE I.ProjectID = P.ProjectID ")
@@ -28,6 +34,171 @@ namespace HireKoro.Forms
             catch (Exception ex)
             {
                 MessageBox.Show("Error loading projects: " + ex.Message);
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string query =$"select I.InvoiceID , P.Title , I.Status , I.Amount , I.IssueDate , I.DueDate  from INVOICES AS I , PROJECTS AS P WHERE I.ProjectID = P.ProjectID and ( InvoiceID like '{this.txtSearch.Text}%' or Title like '{this.txtSearch.Text}%');";
+            this.LoadInvoices(query);
+        }
+
+        internal void ClearALL()
+        {
+            this.txtInvoiceID.Clear();
+            this.cmbprojectn.SelectedIndex = -1;
+            this.txtStatus.Clear();
+            this.txtAmount.Clear();
+            this.txtIssueDate.Clear();
+            this.txtDueDate.Clear();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.ClearALL();
+        }
+
+        private void load_ProjectName(string query = "select  * from PROJECTS")
+        {
+         
+            try
+            {
+                var DT = Main.DB.ExecuteQueryTable(query);
+                this.cmbprojectn.DataSource = DT;
+                this.cmbprojectn.DisplayMember = "Title";
+                this.cmbprojectn.ValueMember = "ProjectID";
+             
+
+            }
+            catch (Exception ex) { MessageBox.Show($"ERROR {ex.Message}"); }
+        }
+
+       
+
+        private void dgvInvoices_DoubleClick(object sender, EventArgs e)
+        {
+           
+            this.txtInvoiceID.Text = this.dgvInvoices.CurrentRow.Cells["InvoiceID"].Value.ToString();
+            this.txtStatus.Text = this.dgvInvoices.CurrentRow.Cells["Status"].Value.ToString();
+            this.txtAmount.Text = this.dgvInvoices.CurrentRow.Cells["Amount"].Value.ToString();
+            this.txtIssueDate.Text = this.dgvInvoices.CurrentRow.Cells["IssueDate"].Value.ToString();
+            this.txtDueDate.Text = this.dgvInvoices.CurrentRow.Cells["DueDate"].Value.ToString();
+
+
+
+
+
+        }
+
+        private void cmbprojectn_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            this.txtInvoiceID.Text = this.dgvInvoices.CurrentRow.Cells["InvoiceID"].Value.ToString();
+            try
+            {
+                string query = $"DELETE FROM INVOICES WHERE InvoiceID = {this.txtInvoiceID.Text};";
+                var response = Main.DB.ExecuteDMLQuery(query);
+                dgvInvoices.DataSource = response;
+                this.LoadInvoices();
+                MessageBox.Show("Project deleted successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting projects: " + ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void CreateProject()
+        {
+            try
+            {
+                string query = $@"INSERT INTO INVOICES (ProjectID, Status, Amount, 
+                                 IssueDate, DueDate) 
+                                 VALUES ( '{cmbprojectn.SelectedValue.ToString()}', '{txtStatus.Text}', 
+                                 '{txtAmount.Text}', '{txtIssueDate.Text}', '{txtDueDate.Text}')";
+
+                Main.DB.ExecuteDMLQuery(query);
+                MessageBox.Show("Project created successfully!");
+                LoadInvoices();
+                ClearALL();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating project: " + ex.Message);
+            }
+        }
+
+        private void UpdateProject()
+        {
+            try
+            {
+                string query = $@"UPDATE INVOICES SET 
+                                
+                                 ProjectID = '{cmbprojectn.SelectedValue.ToString()}',
+                                 Status = '{txtStatus.Text}',
+                                 Amount = '{txtAmount.Text}',
+                                 IssueDate = '{txtIssueDate.Text}',
+                                 DueDate = {txtDueDate.Text}
+                                 WHERE InoviceID = {txtInvoiceID.Text}";
+
+                Main.DB.ExecuteDMLQuery(query);
+                MessageBox.Show("INVOICES updated successfully!");
+                LoadInvoices();
+                ClearALL();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating project: " + ex.Message);
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            bool isUpdateMode = !string.IsNullOrEmpty(txtInvoiceID.Text.Trim());
+
+            if (isUpdateMode)
+            {
+                UpdateProject();
+            }
+            else
+            {
+                CreateProject();
+            }
+        }
+
+        private void cmbprojectn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar ==(char)Keys.Enter)
+            {
+                this.load_ProjectName($"select  * from PROJECTS where Title like '{this.cmbprojectn.Text}%'");
+
+            }
+        }
+
+        private void cmbprojectn_TextChanged(object sender, EventArgs e)
+        {
+            if(this.cmbprojectn.Text == "")
+            {
+                this.load_ProjectName();
+               
             }
         }
     }
--- HireKoro/Forms/AdminUsers.cs
+++ HireKoro/Forms/AdminUsers.cs
@@ -16,18 +16,158 @@ namespace HireKoro.Forms
         {
             InitializeComponent();
             this.LoadUsers();
+            this.load_USER_TYPE();
         }
-        internal void LoadUsers()
+        internal void LoadUsers(string query = "Select * from Users")
+
         {
             try
             {
-                string query = "Select * from Users";
                 var response = Main.DB.ExecuteQueryTable(query);
                 dgvUsers.DataSource = response;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error loading projects: " + ex.Message);
+            }
+        }
+
+       
+
+        internal void Clearall()
+        {
+            this.txtUserID.Clear();
+            this.cmbprojectn.SelectedIndex = -1;
+            this.txtUsername.Clear();
+            this.txtemail.Clear();
+            this.txtPasswordHash.Clear();
+            this.txtname.Clear();
+            this.txtProfilelmg.Clear();
+            this.txtdescription.Clear();   
+            this.txtHourlyrate.Clear();
+            this.txtSkills.Clear();
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.Clearall();
+        }
+
+        private void load_USER_TYPE(string query = "select  * from USER_TYPES")
+        {
+
+            try
+            {
+                var DT = Main.DB.ExecuteQueryTable(query);
+                this.cmbprojectn.DataSource = DT;
+                this.cmbprojectn.DisplayMember = "TypeName";
+                this.cmbprojectn.ValueMember = "UserTypeID";
+
+
+            }
+            catch (Exception ex) { MessageBox.Show($"ERROR {ex.Message}"); }
+        }
+
+        private void dgvUsers_DoubleClick(object sender, EventArgs e)
+        {
+            this.txtUserID.Text = this.dgvUsers.CurrentRow.Cells["UserID"].Value.ToString();
+            this.txtUsername.Text = this.dgvUsers.CurrentRow.Cells["Username"].Value.ToString();
+            this.txtemail.Text = this.dgvUsers.CurrentRow.Cells["Email"].Value.ToString();
+            this.txtPasswordHash.Text = this.dgvUsers.CurrentRow.Cells["PasswordHash"].Value.ToString();
+            this.txtname.Text = this.dgvUsers.CurrentRow.Cells["Nameu"].Value.ToString();
+            this.txtProfilelmg.Text = this.dgvUsers.CurrentRow.Cells["ProfileImg"].Value.ToString();
+            this.txtdescription.Text = this.dgvUsers.CurrentRow.Cells["Description"].Value.ToString();
+            this.txtHourlyrate.Text = this.dgvUsers.CurrentRow.Cells["HourlyRate"].Value.ToString();
+            this.txtSkills.Text = this.dgvUsers.CurrentRow.Cells["Skills"].Value.ToString();
+
+
+
+        }
+
+        private void txtScearch_TextChanged(object sender, EventArgs e)
+        {
+            string query = $"Select * from Users where UserID like '{this.txtScearch.Text}%' or Name like '{this.txtScearch.Text}%'";
+            this.LoadUsers(query);
+        }
+
+        
+        private void CreateProject()
+        {
+            try
+            {
+                string query = $@"INSERT INTO USERS (UserTypeID, Username, Email, 
+                                 PasswordHash, Name,ProfileImg,Description,HourlyRate,Skills,CreatedAt) 
+                                 VALUES ('{cmbprojectn.SelectedValue.ToString()}','{txtUsername.Text}', 
+                                 '{txtemail.Text}','{txtPasswordHash.Text}','{txtname.Text}', '{txtProfilelmg.Text}','{txtdescription.Text}','{txtHourlyrate.Text}','{txtSkills.Text}','{DateTime.Now:yyyy-MM-dd}')";
+
+                Main.DB.ExecuteDMLQuery(query);
+                MessageBox.Show("Project created successfully!");
+                LoadUsers();
+                Clearall();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating project: " + ex.Message);
+            }
+        }
+
+        private void UpdateProject()
+        {
+            try
+            {
+                string query = $@"UPDATE USERS SET 
+                                
+                                 UserTypeID = '{cmbprojectn.SelectedValue.ToString()}',
+                                 Username = '{txtUsername.Text}',
+                                 Email= '{txtemail.Text}',
+                                 PasswordHash = '{txtPasswordHash.Text}',
+                                 Name = '{txtname.Text}',
+                                 ProfileImg ='{txtProfilelmg.Text}',
+                                 Description= '{txtdescription.Text}',
+                                 HourlyRate='{txtHourlyrate.Text}',
+                                 skills='{txtSkills.Text}'
+                                 WHERE UserID = '{txtUserID.Text}'";
+
+                Main.DB.ExecuteDMLQuery(query);
+                MessageBox.Show("Project updated successfully!");
+                LoadUsers();
+                Clearall();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating project: " + ex.Message);
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            bool isUpdateMode = !string.IsNullOrEmpty(txtUserID.Text.Trim());
+
+            if (isUpdateMode)
+            {
+                UpdateProject();
+            }
+            else
+            {
+                CreateProject();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            this.txtUserID.Text = this.dgvUsers.CurrentRow.Cells["UserID"].Value.ToString();
+            try
+            {
+                string query = $"DELETE FROM USERS WHERE UserID = {this.txtUserID.Text};";
+                var response = Main.DB.ExecuteDMLQuery(query);
+                dgvUsers.DataSource = response;
+                this.LoadUsers();
+                MessageBox.Show("USERS deleted successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting projects: " + ex.Message);
             }
         }
     }
